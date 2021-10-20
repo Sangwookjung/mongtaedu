88f05af11a975a81e73c3b55486c4dee9c0e7b35
mogi <perr4343@naver.com>
2021-10-20 09:30:44 +0900

    Second Commit

--- src/main/java/jpastudy/jpashop/domain/item/Item.java
+++ src/main/java/jpastudy/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpastudy.jpashop.domain.item;
 
+import jpastudy.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,17 @@ public abstract class Item {
     //상품 재고
     private int stockQuantity;
 
+    //==비즈니스 로직==//
+    //주문이 취소되어 재고량 복구
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    //주문 후 재고량 감소
+     public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
