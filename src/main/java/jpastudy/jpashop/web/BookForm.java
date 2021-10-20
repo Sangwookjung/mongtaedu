0b5de32be5785029d75c9ef641def8f4e2d20bbe
mogi <perr4343@naver.com>
2021-10-20 17:48:38 +0900

    HTML Item Register

---
+++ src/main/java/jpastudy/jpashop/web/BookForm.java
@@ -0,0 +1,23 @@
+package jpastudy.jpashop.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class BookForm {
+    private Long id;
+
+    @NotBlank(message = "상품이름은 필수 입니다.")
+    private String name;
+
+    private int price;
+
+    @Min(value = 1, message = "상품 재고수량은 1개 이상이어야 한다")
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}
